64e9a16fd547ef27cdcb2bbf52e110d9e493ca8a
Megan <megantech@hotmail.com>
2023-05-13 13:57:36 +0000

    Add: props to function

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const FunctionalGreetingWithProps = () => <h1>Hello from React Props!</h1>
+const FunctionalGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello, {props.greeting} {props.name} {props.age}!</h1>
+}
 
 export default FunctionalGreetingWithProps;
